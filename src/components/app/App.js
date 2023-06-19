ff71eab4f38e9f030f6544b18bf53ad632b4024b
Denis D <Arhcer.php@gmail.com>
2023-06-19 22:20:06 +0700

    initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import data from './utils/data';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
