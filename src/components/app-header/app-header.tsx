4f9789e544b3400c38d6e7981b92e4f472110a75
Barresi <Arhcer.php@gmail.com>
2023-10-08 15:17:37 +0700

    feat: add burger menu

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import smallLogo from '../../images/small-logo.svg';
 import { ProfileIcon, Logo } from '@ya.praktikum/react-developer-burger-ui-components';
@@ -6,40 +6,47 @@ import HeaderNav from './nav/header-nav';
 import style from './app-header.module.scss';
 import burger from '../../images/burger.png';
 import { clx } from '../../utils/set-active-links';
+import Burger from '../burger/burger';
 
 const AppHeader: FC = () => {
+     const [isActiveBurger, setActiveBurger] = useState(false);
      return (
-          <header className={style.header}>
-               <div className={style.wrap}>
-                    <HeaderNav />
+          <>
+               <header className={style.header}>
+                    <div className={style.wrap}>
+                         <HeaderNav />
 
-                    <Link to='/' className={style.logo}>
-                         <Logo />
-                    </Link>
+                         <Link to='/' className={style.logo}>
+                              <Logo />
+                         </Link>
 
-                    <NavLink
-                         to='/profile'
-                         className={({ isActive }) => clx({ [style.profile]: true, [style.profileActive]: isActive })}>
-                         <ProfileIcon type={'secondary'} />
-                         <div
-                              className={`
+                         <NavLink
+                              to='/profile'
+                              className={({ isActive }) =>
+                                   clx({ [style.profile]: true, [style.profileActive]: isActive })
+                              }>
+                              <ProfileIcon type={'secondary'} />
+                              <div
+                                   className={`
                                              ${'text_color_inactive'} 
                                              text text_type_main-default
                                         `}>
-                              Личный кабинет
-                         </div>
-                    </NavLink>
-               </div>
-
-               <div className={style.mobile}>
-                    <Link to='/' className={style.burger}>
-                         <img src={smallLogo} alt='mobileBurger' />
-                    </Link>
-                    <div className={style.burger}>
-                         <img src={burger} alt='burger' />
+                                   Личный кабинет
+                              </div>
+                         </NavLink>
                     </div>
-               </div>
-          </header>
+
+                    <div className={style.mobile}>
+                         <Link to='/' className={style.burger}>
+                              <img src={smallLogo} alt='mobileBurger' />
+                         </Link>
+                         <button className={style.burger} onClick={() => setActiveBurger(true)}>
+                              <img src={burger} alt='burger' />
+                         </button>
+                    </div>
+               </header>
+               <Burger isActive={isActiveBurger} setActive={setActiveBurger} />
+          </>
      );
 };
 
